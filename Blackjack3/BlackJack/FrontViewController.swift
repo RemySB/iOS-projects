af72db771965982a406ba43420e3114228d59202
Student <student@3amac03.local>
2015-03-28 21:56:06 -0400

    Assignment 3
    layout/foundations working, still needs some buffers filled to load
    properly

---
+++ Blackjack3/BlackJack/FrontViewController.swift
@@ -0,0 +1,57 @@
+//
+//  FrontViewController.swift
+//  BlackJack
+//
+//  Created by Student on 2/24/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+    @IBOutlet weak var PlayerStepper: UIStepper!
+
+    @IBOutlet weak var PlayersLabel: UILabel!
+    @IBOutlet weak var DecksLabel: UILabel!
+    
+    @IBOutlet weak var DeckStepper: UIStepper!
+
+    @IBAction func PlayersChanged(sender: AnyObject) {
+        PlayersLabel.text="Players: "+toString(PlayerStepper.value)
+    }
+    @IBAction func DecksChanged(sender: AnyObject) {
+        DecksLabel.text="Decks: "+toString(DeckStepper.value)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "FrontToPlay"
+        {
+            if let destinationVC = segue.destinationViewController as? ViewController{
+                destinationVC.theGame = BlackJackGame(nPlayers: Int(PlayerStepper.value), nDecks: Int(DeckStepper.value))
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Blackjack3/BlackJack/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  BlackJack
+//
+//  Created by Student on 2/17/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numPlayers = 0
+    var numDecks = 0
+    
+    var theGame: BlackJackGame!
+    var nGames = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var betStepper: UIStepper!
+    @IBOutlet weak var betButton: UIButton!
+    @IBOutlet weak var playerCash: UILabel!
+    @IBOutlet weak var dealerBet: UILabel!
+    @IBOutlet weak var playerBet: UILabel!
+    
+    @IBOutlet weak var drawLabel: UILabel!
+    @IBOutlet weak var drawButtons: UISegmentedControl!
+    
+    
+    
+    @IBAction func betButtonPressed() {
+        betAllowedToggle()
+        let betAmt = betStepper.value-betStepper.minimumValue
+        theGame.bet(Int(betAmt))
+        betStepper.minimumValue = betStepper.value
+    }
+    
+    func drawAllowedToggle(){
+        drawLabel.hidden = !drawLabel.hidden
+        drawButtons.enabled = !drawButtons.enabled
+    }
+    
+    func betAllowedToggle(){
+        betButton.enabled = !betButton.enabled
+        betStepper.enabled = !betStepper.enabled
+        betStepper.hidden = !betStepper.hidden
+    }
+    
+    func displayCards(roundOver:Bool){
+        /*theGame.players[theGame.curPlayerIndx].refreshCardLabels(roundOver)
+        theGame.dealer.refreshCardLabels(roundOver)
+        for(var i=0; i < max(theGame.players[theGame.curPlayerIndx].cardLabels.count, theGame.dealer.cardLabels.count); ++i) {
+            //dealerCards.cellForItemAtIndexPath(NSIndexPath(index: i))?.contentView.insertSubview(theGame.dealer.cardLabels[i],atIndex:0)
+            dealerCards.cellForItemAtIndexPath(NSIndexPath(index: i))?.contentView.bringSubviewToFront(theGame.dealer.cardLabels[i])
+            playerCards.cellForItemAtIndexPath(NSIndexPath(index: i))?.contentView.bringSubviewToFront(theGame.players[theGame.curPlayerIndx].cardLabels[i])
+            
+        }*/
+    }
+    
+    func updateDisplayVals(roundOver:Bool){ //roundOver is true when displaying the outcome of the round, false when it's in progress
+        displayCards(roundOver)
+        playerCash.text="$"+String(theGame.players[theGame.curPlayerIndx].cashRemaining)
+        dealerBet.text="$"+String(theGame.dealer.curBet)
+    }
+    
+    func playGame(){
+        
+        
+        nGames++
+        if(nGames==5){
+            theGame.deck.clearDealt()
+            nGames=0
+        }
+        
+        //prompt user to place a bet:
+            
+            betAllowedToggle()
+            while(betButton.enabled){
+                //wait
+            }
+            updateDisplayVals(false)
+            //maybe draw a card? a few?
+        
+        
+        
+        updateDisplayVals(true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "EmbedPlayerCards"{
+            if let destinationVC = segue.destinationViewController as? CardsCollectionViewController{
+                destinationVC.player = theGame.players[theGame.curPlayerIndx]
+            }
+        }
+        else if segue.identifier == "EmbedDealerCards"{
+            if let destinationVC = segue.destinationViewController as? CardsCollectionViewController{
+                destinationVC.player = theGame.dealer
+            }
+        }
+        else if segue.identifier == "EmbedAICards"{
+            if let destinationVC = segue.destinationViewController as? CardsCollectionViewController{
+                destinationVC.player = theGame.AI
+            }
+        }
+    }
+
+
+  
+}
+
